c90846ca88600c0dd879c0b43a611552fbd662c4
caviarcurls <caviarcurls@gmail.com>
2026-05-19 03:08:39 +0100

    Add files via upload

---
+++ products.js
@@ -0,0 +1,493 @@
+const products = {
+
+straightwig:{
+title:"Straight Cambodian Wig",
+type:"13x4 HD Lace Frontal Wig",
+density:"250% Density",
+lace:["13x4 Frontal"],
+image:"images/straightwig-1.jpg",
+description:"Luxury Cambodian straight wig.",
+prices:{"13x4 Frontal":{"16":100,"18":115,"20":130,"22":155,"24":170,"26":185,"28":200,"30":215}}
+},
+
+bodywavewig:{
+title:"Body Wave Cambodian Wig",
+type:"13x4 HD Lace Frontal Wig",
+density:"250% Density",
+lace:["13x4 Frontal"],
+image:"images/bodywavewig-1.png",
+description:"Luxury Cambodian body wave wig.",
+prices:{"13x4 Frontal":{"16":100,"18":115,"20":130,"22":155,"24":170,"26":185,"28":200,"30":215}}
+},
+
+deepwavewig:{
+title:"Deep Wave Cambodian Wig",
+type:"13x4 HD Lace Frontal Wig",
+density:"250% Density",
+lace:["13x4 Frontal"],
+image:"images/deepwavewig-1.jpg",
+description:"Luxury Cambodian deep wave wig.",
+prices:{"13x4 Frontal":{"16":110,"18":120,"20":135,"22":160,"24":175,"26":190,"28":205,"30":220}}
+},
+
+loosewavewig:{
+title:"Loose Wave Cambodian Wig",
+type:"13x4 HD Lace Frontal Wig",
+density:"250% Density",
+lace:["13x4 Frontal"],
+image:"images/loosewavewig-1.jpg",
+description:"Luxury Cambodian loose wave wig.",
+prices:{"13x4 Frontal":{"16":110,"18":120,"20":135,"22":160,"24":175,"26":190,"28":205,"30":220}}
+},
+
+waterwavewig:{
+title:"Water Wave Cambodian Wig",
+type:"13x4 HD Lace Frontal Wig",
+density:"250% Density",
+lace:["13x4 Frontal"],
+image:"images/waterwavewig-1.png",
+description:"Luxury Cambodian water wave wig.",
+prices:{"13x4 Frontal":{"16":110,"18":120,"20":135,"22":160,"24":175,"26":190,"28":205,"30":220}}
+},
+
+rawvietnamese:{
+title:"RAW Vietnamese Wig",
+type:"RAW Vietnamese Hair",
+lace:["13x4 HD","5x5 HD","2x6 HD"],
+image:"images/rawvietnamese-1.png",
+description:"Luxury RAW Vietnamese wig.",
+prices:{
+"13x4 HD":{"20":125,"22":135,"24":160,"26":190,"28":220,"30":245,"32":285,"34":330},
+"5x5 HD":{"20":105,"22":120,"24":145,"26":170,"28":200,"30":225,"32":260,"34":300},
+"2x6 HD":{"20":100,"22":115,"24":140,"26":165,"28":195,"30":220,"32":255,"34":295}
+}
+},
+
+ssdbrownbodywave:{
+title:"SSD Brown Body Wave Wig",
+type:"Luxury Colored Wig",
+lace:["13x6 HD","5x5 HD"],
+image:"images/ssdbrownbodywave-1.jpg",
+description:"Luxury SSD brown body wave wig.",
+prices:{
+"13x6 HD":{"16":145,"18":150,"20":158,"22":163,"24":170,"26":185},
+"5x5 HD":{"16":135,"18":145,"20":150,"22":160,"24":165,"26":175}
+}
+},
+
+ssdbalayagebodywave:{
+title:"SSD Balayage Body Wave Wig",
+type:"Luxury Colored Wig",
+lace:["13x6 HD","5x5 HD"],
+image:"images/ssdbalayagebodywave-1.jpg",
+description:"Luxury SSD balayage wig.",
+prices:{
+"13x6 HD":{"16":145,"18":150,"20":158,"22":163,"24":170,"26":185},
+"5x5 HD":{"16":135,"18":145,"20":150,"22":160,"24":165,"26":175}
+}
+},
+
+ssdwaterwaveburgundy:{
+title:"SSD Waterwave Burgundy Wig",
+type:"Luxury Colored Wig",
+lace:["13x6 HD"],
+image:"images/ssdwaterwaveburgundy-1.png",
+description:"Luxury SSD burgundy waterwave wig.",
+prices:{"13x6 HD":{"16":110,"18":120,"20":135,"22":160,"24":175,"26":190,"28":205,"30":220}}
+},
+
+ssdwaterwavebrownhighlight:{
+title:"SSD Waterwave Brown Highlight Wig",
+type:"Luxury Highlight Wig",
+lace:["13x6 HD"],
+image:"images/ssdwaterwavebrownhighlight-1.jpg",
+description:"Luxury SSD brown highlight wig.",
+prices:{"13x6 HD":{"16":110,"18":120,"20":135,"22":160,"24":175,"26":190,"28":205,"30":220}}
+},
+
+blondehighlight:{
+title:"Blonde Highlight Wig",
+type:"Luxury Blonde Highlight Wig",
+density:"250% Density",
+lace:["13x4 Lace"],
+image:"images/blondehighlight-1.png",
+description:"Luxury blonde highlight wig.",
+prices:{"13x4 Lace":{"16":115,"18":130,"20":150,"22":175,"24":210,"26":250,"28":275,"30":350}}
+},
+
+bobburgundy:{
+title:"Vietnamese Burgundy Bobs",
+type:"Luxury Bob Wig",
+lace:["13x6 HD","5x5 HD"],
+image:"images/bobburgundy-1.png",
+description:"Luxury burgundy bob wig.",
+prices:{"13x6 HD":{"8":105,"10":125,"12":145},"5x5 HD":{"8":100,"10":110,"12":120}}
+},
+
+bobbrown:{
+title:"Vietnamese Brown Bobs",
+type:"Luxury Bob Wig",
+lace:["13x6 HD","5x5 HD"],
+image:"images/bobbrown-1.jpg",
+description:"Luxury brown bob wig.",
+prices:{"13x6 HD":{"8":105,"10":125,"12":145},"5x5 HD":{"8":100,"10":110,"12":120}}
+},
+
+bobblack:{
+title:"Vietnamese Black Bobs",
+type:"Luxury Bob Wig",
+lace:["13x6 HD","5x5 HD"],
+image:"images/bobblack-1.png",
+description:"Luxury black bob wig.",
+prices:{"13x6 HD":{"8":105,"10":125,"12":145},"5x5 HD":{"8":100,"10":110,"12":120}}
+},
+
+bobblonde:{
+title:"Vietnamese Blonde Bobs",
+type:"Luxury Blonde Bob Wig",
+lace:["13x6 HD","5x5 HD"],
+image:"images/bobblonde-1.png",
+description:"Luxury blonde bob wig.",
+prices:{"13x6 HD":{"8":105,"10":125,"12":145},"5x5 HD":{"8":100,"10":110,"12":120}}
+},
+
+straightbundles:{
+title:"Straight Bundles 14A",
+type:"Luxury Cambodian Bundles",
+lace:["1 Bundle","2 Bundles","3 Bundles","4 Bundles","5 Bundles"],
+image:"images/straightbundles-1.png",
+description:"Luxury straight bundles.",
+prices:{
+"1 Bundle":{"14":38,"16":46,"18":56,"20":65,"22":75,"24":84,"26":90,"28":100,"30":110,"32":120},
+"2 Bundles":{"14":76,"16":92,"18":112,"20":130,"22":150,"24":168,"26":180,"28":200,"30":220,"32":240},
+"3 Bundles":{"14":114,"16":138,"18":168,"20":195,"22":225,"24":252,"26":270,"28":300,"30":330,"32":360},
+"4 Bundles":{"14":152,"16":184,"18":224,"20":260,"22":300,"24":336,"26":360,"28":400,"30":440,"32":480},
+"5 Bundles":{"14":190,"16":230,"18":280,"20":325,"22":375,"24":420,"26":450,"28":500,"30":550,"32":600}
+}
+},
+
+rawstraightbundles:{
+title:"RAW Straight Bundles",
+type:"Luxury RAW Bundles",
+lace:["1 Bundle","2 Bundles","3 Bundles","4 Bundles","5 Bundles"],
+image:"images/rawstraightbundles-1.png",
+description:"Luxury RAW straight bundles.",
+prices:{
+"1 Bundle":{"14":45,"16":50,"18":60,"20":70,"22":80,"24":90,"26":100,"28":110,"30":120,"32":130},
+"2 Bundles":{"14":90,"16":100,"18":120,"20":140,"22":160,"24":180,"26":200,"28":220,"30":240,"32":260},
+"3 Bundles":{"14":135,"16":150,"18":180,"20":210,"22":240,"24":270,"26":300,"28":330,"30":360,"32":390},
+"4 Bundles":{"14":180,"16":200,"18":240,"20":280,"22":320,"24":360,"26":400,"28":440,"30":480,"32":520},
+"5 Bundles":{"14":225,"16":250,"18":300,"20":350,"22":400,"24":450,"26":500,"28":550,"30":600,"32":650}
+}
+},
+
+bodywavebundles:{
+title:"Body Wave Bundles 14A",
+type:"Luxury Cambodian Bundles",
+lace:["1 Bundle","2 Bundles","3 Bundles","4 Bundles","5 Bundles"],
+image:"images/bodywavebundles-1.png",
+description:"Luxury body wave bundles.",
+prices:{
+"1 Bundle":{"14":38,"16":46,"18":56,"20":65,"22":75,"24":84,"26":90,"28":100,"30":110,"32":120},
+"2 Bundles":{"14":76,"16":92,"18":112,"20":130,"22":150,"24":168,"26":180,"28":200,"30":220,"32":240},
+"3 Bundles":{"14":114,"16":138,"18":168,"20":195,"22":225,"24":252,"26":270,"28":300,"30":330,"32":360},
+"4 Bundles":{"14":152,"16":184,"18":224,"20":260,"22":300,"24":336,"26":360,"28":400,"30":440,"32":480},
+"5 Bundles":{"14":190,"16":230,"18":280,"20":325,"22":375,"24":420,"26":450,"28":500,"30":550,"32":600}
+}
+},
+
+rawbodywavebundles:{
+title:"RAW Body Wave Bundles",
+type:"Luxury RAW Bundles",
+lace:["1 Bundle","2 Bundles","3 Bundles","4 Bundles","5 Bundles"],
+image:"images/rawbodywavebundles-1.png",
+description:"Luxury RAW body wave bundles.",
+prices:{
+"1 Bundle":{"14":45,"16":50,"18":60,"20":70,"22":80,"24":90,"26":100,"28":110,"30":120,"32":130},
+"2 Bundles":{"14":90,"16":100,"18":120,"20":140,"22":160,"24":180,"26":200,"28":220,"30":240,"32":260},
+"3 Bundles":{"14":135,"16":150,"18":180,"20":210,"22":240,"24":270,"26":300,"28":330,"30":360,"32":390},
+"4 Bundles":{"14":180,"16":200,"18":240,"20":280,"22":320,"24":360,"26":400,"28":440,"30":480,"32":520},
+"5 Bundles":{"14":225,"16":250,"18":300,"20":350,"22":400,"24":450,"26":500,"28":550,"30":600,"32":650}
+}
+},
+
+hd13x6frontal:{
+title:"13x6 HD Frontal",
+type:"HD Lace Frontal",
+lace:["13x6 HD"],
+image:"images/hd13x6frontal-1.jpg",
+description:"Luxury HD frontal.",
+prices:{"13x6 HD":{"14":55,"16":65,"18":75,"20":85,"22":100}}
+},
+
+hd13x4frontal:{
+title:"13x4 HD Frontal",
+type:"HD Lace Frontal",
+lace:["13x4 HD"],
+image:"images/hd13x4frontal-1.jpg",
+description:"Luxury HD frontal.",
+prices:{"13x4 HD":{"14":50,"16":60,"18":70,"20":80,"22":90}}
+}
+
+};
+
+const params = new URLSearchParams(window.location.search);
+const productKey = params.get("product");
+const product = products[productKey];
+
+if(product){
+  const galleryByProduct = {
+    straightwig:[
+      "images/straightwig-1.jpg",
+      "images/straightwig-2.jpg",
+      "images/straightwig-3.jpg",
+      "images/straightwig-4.jpg"
+    ],
+    bodywavewig:[
+      "images/bodywavewig-1.png",
+      "images/bodywavewig-2.png",
+      "images/bodywavewig-3.png",
+      "images/bodywavewig-4.png"
+    ],
+    deepwavewig:[
+      "images/deepwavewig-1.jpg",
+      "images/deepwavewig-2.jpg",
+      "images/deepwavewig-3.jpg",
+      "images/deepwavewig-4.jpg"
+    ],
+    loosewavewig:[
+      "images/loosewavewig-1.jpg",
+      "images/loosewavewig-2.jpg",
+      "images/loosewavewig-3.jpg",
+      "images/loosewavewig-4.jpg"
+    ],
+    waterwavewig:[
+      "images/waterwavewig-1.png",
+      "images/waterwavewig-2.png",
+      "images/waterwavewig-3.jpg",
+      "images/waterwavewig-4.png"
+    ],
+    rawvietnamese:[
+      "images/rawvietnamese-1.png",
+      "images/rawvietnamese-2.png",
+      "images/rawvietnamese-3.png",
+      "images/rawvietnamese-4.png"
+    ],
+    ssdbrownbodywave:[
+      "images/ssdbrownbodywave-1.jpg",
+      "images/ssdbrownbodywave-2.jpg",
+      "images/ssdbrownbodywave-3.jpg",
+      "images/ssdbrownbodywave-4.jpg"
+    ],
+    ssdbalayagebodywave:[
+      "images/ssdbalayagebodywave-1.jpg",
+      "images/ssdbalayagebodywave-2.jpg",
+      "images/ssdbalayagebodywave-3.jpg",
+      "images/ssdbalayagebodywave-4.jpg"
+    ],
+    ssdwaterwaveburgundy:[
+      "images/ssdwaterwaveburgundy-1.png",
+      "images/ssdwaterwaveburgundy-2.png",
+      "images/ssdwaterwaveburgundy-3.jpg",
+      "images/ssdwaterwaveburgundy-4.png"
+    ],
+    ssdwaterwavebrownhighlight:[
+      "images/ssdwaterwavebrownhighlight-1.jpg",
+      "images/ssdwaterwavebrownhighlight-2.jpg",
+      "images/ssdwaterwavebrownhighlight-3.jpg",
+      "images/ssdwaterwavebrownhighlight-4.jpg"
+    ],
+    blondehighlight:[
+      "images/blondehighlight-1.png",
+      "images/blondehighlight-2.png",
+      "images/blondehighlight-3.png",
+      "images/blondehighlight-4.png"
+    ],
+    bobburgundy:[
+      "images/bobburgundy-1.png",
+      "images/bobburgundy-2.png",
+      "images/bobburgundy-3.jpg",
+      "images/bobburgundy-4.png"
+    ],
+    bobbrown:[
+      "images/bobbrown-1.jpg",
+      "images/bobbrown-2.jpg",
+      "images/bobbrown-3.jpg",
+      "images/bobbrown-4.jpg"
+    ],
+    bobblack:[
+      "images/bobblack-1.png",
+      "images/bobblack-2.png",
+      "images/bobblack-3.png",
+      "images/bobblack-4.png"
+    ],
+    bobblonde:[
+      "images/bobblonde-1.png",
+      "images/bobblonde-2.png",
+      "images/bobblonde-3.jpg",
+      "images/bobblonde-4.jpg"
+    ],
+    straightbundles:[
+      "images/straightbundles-1.png",
+      "images/straightbundles-2.png",
+      "images/straightbundles-3.png",
+      "images/straightbundles-4.jpg"
+    ],
+    rawstraightbundles:[
+      "images/rawstraightbundles-1.png",
+      "images/rawstraightbundles-2.png",
+      "images/rawstraightbundles-3.png",
+      "images/rawstraightbundles-4.png"
+    ],
+    bodywavebundles:[
+      "images/bodywavebundles-1.png",
+      "images/bodywavebundles-2.png",
+      "images/bodywavebundles-3.png",
+      "images/bodywavebundles-4.png"
+    ],
+    rawbodywavebundles:[
+      "images/rawbodywavebundles-1.png",
+      "images/rawbodywavebundles-2.png",
+      "images/rawbodywavebundles-3.jpg",
+      "images/rawbodywavebundles-4.jpg"
+    ],
+    hd13x6frontal:[
+      "images/hd13x6frontal-1.jpg",
+      "images/hd13x6frontal-2.jpg",
+      "images/hd13x6frontal-3.jpg",
+      "images/hd13x6frontal-4.jpg"
+    ],
+    hd13x4frontal:[
+      "images/hd13x4frontal-1.jpg",
+      "images/hd13x4frontal-2.jpg",
+      "images/hd13x4frontal-3.jpg",
+      "images/hd13x4frontal-4.jpg"
+    ]
+  };
+
+  const fallbackGallery = [
+    product.image,
+    "images/product-photo-01.jpg",
+    "images/product-photo-02.jpg",
+    "images/product-photo-03.jpg"
+  ];
+
+  const galleryImages = [...new Set([
+    ...(galleryByProduct[productKey] || fallbackGallery),
+    ...fallbackGallery
+  ])].filter(Boolean).slice(0,4);
+
+  document.getElementById("product-title").innerText = product.title;
+  document.getElementById("product-type").innerText = product.density ? `${product.type} - ${product.density}` : product.type;
+  document.getElementById("product-description").innerText = product.description;
+  const productImage = document.getElementById("product-image");
+  productImage.src = galleryImages[0] || product.image;
+  productImage.alt = product.title;
+  const productGallery = document.getElementById("product-gallery");
+
+  if(productGallery){
+    productGallery.innerHTML = "";
+
+    galleryImages.forEach((src,index)=>{
+      const button = document.createElement("button");
+      button.type = "button";
+      button.className = index === 0 ? "active" : "";
+      button.setAttribute("aria-label", `View product image ${index + 1}`);
+      button.innerHTML = `<img src="${src}" alt="${product.title} view ${index + 1}">`;
+
+      button.addEventListener("click",()=>{
+        productImage.src = src;
+        productGallery.querySelectorAll("button").forEach(item=>item.classList.remove("active"));
+        button.classList.add("active");
+      });
+
+      productGallery.appendChild(button);
+    });
+  }
+
+  const laceSelect = document.getElementById("lace-select");
+  const lengthSelect = document.getElementById("length-select");
+  const typeLabel = document.getElementById("type-label");
+  const isBundleProduct = productKey && productKey.includes("bundles");
+
+  if(typeLabel && isBundleProduct){
+    typeLabel.innerText = "Select Bundle Amount";
+  }
+
+  laceSelect.innerHTML = "";
+  lengthSelect.innerHTML = "";
+
+  product.lace.forEach(lace=>{
+    const option = document.createElement("option");
+    option.value = lace;
+    option.textContent = lace;
+    laceSelect.appendChild(option);
+  });
+
+  function populateLengths(){
+    lengthSelect.innerHTML = "";
+    const selectedLace = laceSelect.value;
+    Object.keys(product.prices[selectedLace]).forEach(length=>{
+      const option = document.createElement("option");
+      option.value = length;
+      option.textContent = `${length}\"`;
+      lengthSelect.appendChild(option);
+    });
+    updatePrice();
+  }
+
+  function getSelectedProduct(){
+    const selectedLace = laceSelect.value;
+    const selectedLength = lengthSelect.value;
+    const bundleCount = isBundleProduct
+      ? Number((selectedLace.match(/\d+/) || ["1"])[0])
+      : 1;
+    const singleBundlePrice = isBundleProduct && product.prices["1 Bundle"]
+      ? Number(product.prices["1 Bundle"][selectedLength])
+      : null;
+    const finalPrice = singleBundlePrice
+      ? singleBundlePrice * bundleCount
+      : Number(product.prices[selectedLace][selectedLength]);
+
+    return {
+      key: productKey,
+      title: product.title,
+      name: `${product.title} / ${selectedLace} / ${selectedLength}\"`,
+      type: selectedLace,
+      length: `${selectedLength}\"`,
+      price: finalPrice,
+      image: productImage.src || product.image,
+      quantity: 1
+    };
+  }
+
+  function updatePrice(){
+    const selected = getSelectedProduct();
+    document.getElementById("product-price").innerText = `£${selected.price}`;
+  }
+
+  laceSelect.addEventListener("change", populateLengths);
+  lengthSelect.addEventListener("change", updatePrice);
+  populateLengths();
+
+  const cartButton = document.querySelector(".luxury-cart-btn");
+  cartButton.addEventListener("click", ()=>{
+    const selected = getSelectedProduct();
+    const cart = JSON.parse(localStorage.getItem("caviarCart") || "[]");
+    const existingItem = cart.find(item =>
+      item.title === selected.title &&
+      item.type === selected.type &&
+      String(item.length) === String(selected.length)
+    );
+
+    if(existingItem){
+      existingItem.quantity = (Number(existingItem.quantity) || 1) + 1;
+    } else {
+      cart.push(selected);
+    }
+
+    localStorage.setItem("caviarCart", JSON.stringify(cart));
+    localStorage.setItem("checkoutProduct", JSON.stringify(selected));
+    window.location.href = "cart.html";
+  });
+}
